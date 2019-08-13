e44596049b28b9c22c3bd6d4bb9c2859273f808a
Alexandru Curca <alexandru.adrian.curca@gmail.com>
2019-08-13 15:04:33 +0300

    NoMatch component now has a button to send the user back to the previous link

--- shopery/src/components/no-match/NoMatch.tsx
+++ shopery/src/components/no-match/NoMatch.tsx
@@ -1,17 +1,26 @@
 import React from "react";
-
+// Import components
 import Logo from "../logo/Logo";
 import Footer from "../footer/Footer";
 import StyledNotFound from "./no-match-styles";
+// Import prop types for NoMatch component
+import { RouteComponentProps } from "react-router-dom";
+// Import styled components
+import { StyledButton } from "../common-styled-components/common";
 
-const NoMatch: React.FC = (): JSX.Element => (
-  <StyledNotFound>
-    <Logo />
-    <h2>
-      We are sorry! Looks like we couldn't find the page you were looking for!
-    </h2>
-    <Footer />
-  </StyledNotFound>
-);
+const NoMatch: React.FC<RouteComponentProps> = ({ history }): JSX.Element => {
+  return (
+    <StyledNotFound>
+      <Logo />
+      <h2>
+        We are sorry! Looks like we couldn't find the page you were looking for!
+      </h2>
+      <StyledButton formButton onClick={() => history.goBack()}>
+        Go Back
+      </StyledButton>
+      <Footer />
+    </StyledNotFound>
+  );
+};
 
 export default NoMatch;
